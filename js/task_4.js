c78bd33d6fd198bf31376ef727ebb8e5d3bfdbc8
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-10-26 01:41:05 +0400

    4done

--- js/task_4.js
+++ js/task_4.js
@@ -1,38 +1,38 @@
 
  
-  class StringBuilder {
+ class StringBuilder {
     _value = "";
-  construktor (value){
+  constructor (value){
   this._value = value;
+   
   }
 get value(){
 return this._value;}
 
 
 append(str){
-this._value = this._value + str;
+return this._value = `${this._value}${str}`;
 }
 
-prepend(str) {
-return this._value = str + this._value;
+prepend(str){
+ return this._value = `${str}${this._value}`;
     }
     pad(str) {
-      this.prepend(str);
       this.append(str);
+     this.prepend(str);
     }
   }
-  console.log(typeof StringBuilder);
-  // 'function'
-  
-  
-  const builder = new StringBuilder('.');
-  
-  builder.append('^');
-  console.log(builder.value); // '.^'
-  
-  builder.prepend('^');
-  console.log(builder.value); // '^.^'
-  
-  builder.pad('=');
-  console.log(builder.value); // '=^.^='
-  
+console.log(typeof StringBuilder);
+// 'function'
+
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
