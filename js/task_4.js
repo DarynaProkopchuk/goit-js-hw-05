23c72c79f34c323ec2fbed374e6ed9cba8a8e20e
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-10-26 01:02:01 +0400

    1.2.3.done

---
+++ js/task_4.js
@@ -0,0 +1,38 @@
+
+ 
+  class StringBuilder {
+    _value = "";
+  construktor (value){
+  this._value = value;
+  }
+get value(){
+return this._value;}
+
+
+append(str){
+this._value = this._value + str;
+}
+
+prepend(str) {
+return this._value = str + this._value;
+    }
+    pad(str) {
+      this.prepend(str);
+      this.append(str);
+    }
+  }
+  console.log(typeof StringBuilder);
+  // 'function'
+  
+  
+  const builder = new StringBuilder('.');
+  
+  builder.append('^');
+  console.log(builder.value); // '.^'
+  
+  builder.prepend('^');
+  console.log(builder.value); // '^.^'
+  
+  builder.pad('=');
+  console.log(builder.value); // '=^.^='
+  
